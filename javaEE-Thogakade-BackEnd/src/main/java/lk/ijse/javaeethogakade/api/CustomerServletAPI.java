e8947810dee102a9a07286a48e3fa7144840468d
LahiruHarshana <lharshana2002@gmail.com>
2024-02-02 23:57:28 +0530

    first commit

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/api/CustomerServletAPI.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/api/CustomerServletAPI.java
@@ -61,7 +61,7 @@ public class CustomerServletAPI extends HttpServlet {
         response.addHeader("Access-Control-Allow-Origin", "http://localhost:63342");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-        resp.addHeader("Access-Control-Max-Age", "3600");
+        response.addHeader("Access-Control-Max-Age", "3600");
         try {
             String sql = "SELECT * FROM customer WHERE cusID=?";
             ResultSet rst = SQLUtil.execute(sql, customerId);
