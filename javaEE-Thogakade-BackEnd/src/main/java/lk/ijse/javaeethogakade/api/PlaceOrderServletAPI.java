68bea15ba728923798c9bb7c5b4db73ecd7695db
LahiruHarshana <lharshana2002@gmail.com>
2024-02-03 23:06:52 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/api/PlaceOrderServletAPI.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/api/PlaceOrderServletAPI.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.javaeethogakade.util.SQLUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
