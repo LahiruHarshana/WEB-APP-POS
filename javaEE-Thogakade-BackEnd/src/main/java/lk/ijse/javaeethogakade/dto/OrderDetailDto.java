a60cb7299c37366d5eb4250bb7f645605125c680
LahiruHarshana <lharshana2002@gmail.com>
2024-02-03 23:08:26 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dto/OrderDetailDto.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dto/OrderDetailDto.java
@@ -1,5 +1,9 @@
 package lk.ijse.javaeethogakade.dto;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDetailDto {
     private String itemCode;
     private String orderID;
