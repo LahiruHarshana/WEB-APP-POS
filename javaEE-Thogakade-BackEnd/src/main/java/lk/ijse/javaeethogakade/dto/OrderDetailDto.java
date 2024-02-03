ca3acdd1fccb754fc3317a49a9ff26acceb41237
LahiruHarshana <lharshana2002@gmail.com>
2024-02-03 23:08:00 +0530

    updated

---
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dto/OrderDetailDto.java
@@ -0,0 +1,8 @@
+package lk.ijse.javaeethogakade.dto;
+
+public class OrderDetailDto {
+    private String itemCode;
+    private String orderID;
+    private int quantity;
+    private double itemPrice;
+}
