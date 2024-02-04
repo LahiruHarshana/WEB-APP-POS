55d5e7d95d80e0fb20c8517cb8ac89aad5e03fcb
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 12:26:03 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/PurchaseOrderBO.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/PurchaseOrderBO.java
@@ -1,7 +1,30 @@
 package lk.ijse.javaeethogakade.bo.custom;
 
 import lk.ijse.javaeethogakade.bo.SuperBO;
+import lk.ijse.javaeethogakade.dto.CustomerDto;
+import lk.ijse.javaeethogakade.dto.ItemDTO;
+import lk.ijse.javaeethogakade.dto.OrderDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface PurchaseOrderBO extends SuperBO {
 
+    public CustomerDto searchCustomer(String id) throws SQLException, ClassNotFoundException ;
+
+    public ItemDTO searchItem(String code) throws SQLException, ClassNotFoundException ;
+
+    public boolean existItem(String code) throws SQLException, ClassNotFoundException;
+
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException ;
+
+    public String generateOrderID() throws SQLException, ClassNotFoundException ;
+
+    public ArrayList<CustomerDto> getAllCustomers() throws SQLException, ClassNotFoundException;
+
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException;
+
+    public boolean purchaseOrder(OrderDto dto)throws SQLException, ClassNotFoundException;
+
+    public ItemDTO findItem(String code)throws SQLException, ClassNotFoundException;
 }
