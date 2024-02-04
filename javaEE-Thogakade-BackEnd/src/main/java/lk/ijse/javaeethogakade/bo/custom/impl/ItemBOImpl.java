69b92dca1193fd7e10aa578698d2c3470e49493e
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 12:28:49 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/ItemBOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/ItemBOImpl.java
@@ -1,4 +1,39 @@
 package lk.ijse.javaeethogakade.bo.custom.impl;
 
-public class ItemBOImpl {
+import lk.ijse.javaeethogakade.bo.custom.ItemBO;
+import lk.ijse.javaeethogakade.dto.ItemDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemBOImpl implements ItemBO {
+    @Override
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean existItem(String code) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewCode() throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
