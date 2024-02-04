fc5ae6a084ca6e0a6c3d9fdedbff4552287c27ef
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:29:14 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/Items.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/Items.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Item {
+public class Items {
     private String ItemCode;
     private String ItemName;
     private double ItemPrice;
