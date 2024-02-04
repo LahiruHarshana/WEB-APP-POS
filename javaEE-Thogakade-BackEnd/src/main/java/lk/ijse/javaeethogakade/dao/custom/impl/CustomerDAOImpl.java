7eaafc87747edf55f1ef450045cee35f94c03180
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:39:29 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/CustomerDAOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/CustomerDAOImpl.java
@@ -1,4 +1,44 @@
 package lk.ijse.javaeethogakade.dao.custom.impl;
 
-public class CustomerDAOImpl{
+import lk.ijse.javaeethogakade.dao.custom.CustomerDAO;
+import lk.ijse.javaeethogakade.entity.Customer;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public ArrayList<Customer> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean add(Customer entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Customer entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Customer search(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
