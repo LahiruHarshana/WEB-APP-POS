5a266044ba1d4d65fdba9bea15562860da9b3cdd
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:25:50 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/Customer.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/Customer.java
@@ -1,6 +1,7 @@
 package lk.ijse.javaeethogakade.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 public class Customer {
     private String cusID;
     private String cusName;
