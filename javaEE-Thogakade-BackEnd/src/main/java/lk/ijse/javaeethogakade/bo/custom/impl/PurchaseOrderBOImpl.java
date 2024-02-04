08cb88550d87145e1dc07cd1d6c0f4a418a87854
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 12:28:31 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/PurchaseOrderBOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/PurchaseOrderBOImpl.java
@@ -1,4 +1,56 @@
 package lk.ijse.javaeethogakade.bo.custom.impl;
 
-public class PurchaseOrderBOImpl {
+import lk.ijse.javaeethogakade.bo.custom.PurchaseOrderBO;
+import lk.ijse.javaeethogakade.dto.CustomerDto;
+import lk.ijse.javaeethogakade.dto.ItemDTO;
+import lk.ijse.javaeethogakade.dto.OrderDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class PurchaseOrderBOImpl implements PurchaseOrderBO {
+    @Override
+    public CustomerDto searchCustomer(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ItemDTO searchItem(String code) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean existItem(String code) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateOrderID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<CustomerDto> getAllCustomers() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean purchaseOrder(OrderDto dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public ItemDTO findItem(String code) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
