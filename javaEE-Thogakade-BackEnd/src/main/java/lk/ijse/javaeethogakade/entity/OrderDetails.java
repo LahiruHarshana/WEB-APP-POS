7e3cfb80484dd34e74331521220fb1e4b3a23871
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:32:38 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/OrderDetails.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/OrderDetails.java
@@ -1,4 +1,8 @@
 package lk.ijse.javaeethogakade.entity;
 
 public class OrderDetails {
+    private String itemCode;
+    private String orderID;
+    private int quantity;
+    private double itemPrice;
 }
