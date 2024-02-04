b17fd250b4c14e11fe11aca6df61fc440366dceb
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:17:55 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/CrudDAO.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/CrudDAO.java
@@ -1,4 +1,14 @@
 package lk.ijse.javaeethogakade.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface CrudDAO <T> extends SuperDAO{
+    public ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+    public boolean add(T entity) throws SQLException, ClassNotFoundException;
+    public boolean update(T entity) throws SQLException, ClassNotFoundException;
+    public boolean exist(String id) throws SQLException, ClassNotFoundException;
+    public String generateNewID() throws SQLException, ClassNotFoundException;
+    public boolean delete(String id) throws SQLException, ClassNotFoundException;
+    public T search(String id) throws SQLException, ClassNotFoundException;
 }
