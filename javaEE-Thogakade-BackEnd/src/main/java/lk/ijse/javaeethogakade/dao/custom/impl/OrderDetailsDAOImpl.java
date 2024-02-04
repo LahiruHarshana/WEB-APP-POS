b6bb0a0ad70ca05dc0e40644dcb75873330f3678
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:38:44 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/OrderDetailsDAOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/OrderDetailsDAOImpl.java
@@ -1,4 +1,44 @@
 package lk.ijse.javaeethogakade.dao.custom.impl;
 
-public class OrderDetailsDAOImpl {
+import lk.ijse.javaeethogakade.dao.custom.OrderDetailsDAO;
+import lk.ijse.javaeethogakade.entity.OrderDetails;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class OrderDetailsDAOImpl implements OrderDetailsDAO {
+    @Override
+    public ArrayList<OrderDetails> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean add(OrderDetails entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(OrderDetails entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public OrderDetails search(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
