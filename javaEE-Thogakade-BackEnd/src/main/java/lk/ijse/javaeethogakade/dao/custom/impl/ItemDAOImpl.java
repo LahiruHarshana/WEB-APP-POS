07fb36630906b0eda7c3e86cf67bcee0fca81763
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:39:16 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/ItemDAOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/ItemDAOImpl.java
@@ -1,4 +1,44 @@
 package lk.ijse.javaeethogakade.dao.custom.impl;
 
-public class ItemDAOImpl {
+import lk.ijse.javaeethogakade.dao.custom.ItemDAO;
+import lk.ijse.javaeethogakade.entity.Items;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemDAOImpl implements ItemDAO {
+    @Override
+    public ArrayList<Items> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean add(Items entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Items entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Items search(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
