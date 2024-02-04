a08893947cdcd9ec1706efd43523ec0b3321789a
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:39:03 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/OrderDAOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/OrderDAOImpl.java
@@ -1,6 +1,44 @@
 package lk.ijse.javaeethogakade.dao.custom.impl;
 
 import lk.ijse.javaeethogakade.dao.custom.OrderDAO;
+import lk.ijse.javaeethogakade.entity.Orders;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class OrderDAOImpl implements OrderDAO {
+    @Override
+    public ArrayList<Orders> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean add(Orders entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Orders entity) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean exist(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Orders search(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
