d38472ae060a6c2b0105dd2431c08a9a5281629e
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 12:25:06 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/CustomerBO.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/CustomerBO.java
@@ -1,6 +1,21 @@
 package lk.ijse.javaeethogakade.bo.custom;
 
 import lk.ijse.javaeethogakade.bo.SuperBO;
+import lk.ijse.javaeethogakade.dto.CustomerDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface CustomerBO extends SuperBO {
+    public ArrayList<CustomerDto> getAllCustomers() throws SQLException, ClassNotFoundException;
+
+    public boolean addCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException ;
+
+    public boolean updateCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException ;
+
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException;
+
+    public boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException;
+
+    public String generateNewCustomerID() throws SQLException, ClassNotFoundException;
 }
--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/ItemBO.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/ItemBO.java
@@ -1,6 +1,21 @@
 package lk.ijse.javaeethogakade.bo.custom;
 
 import lk.ijse.javaeethogakade.bo.SuperBO;
+import lk.ijse.javaeethogakade.dto.ItemDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface ItemBO extends SuperBO {
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException;
+
+    public boolean deleteItem(String code) throws SQLException, ClassNotFoundException ;
+
+    public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean existItem(String code) throws SQLException, ClassNotFoundException ;
+
+    public String generateNewCode() throws SQLException, ClassNotFoundException ;
 }
