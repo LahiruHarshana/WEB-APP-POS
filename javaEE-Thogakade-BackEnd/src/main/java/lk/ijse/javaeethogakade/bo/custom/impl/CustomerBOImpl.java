82b744119c73348da6e1ca04391e9e4a826ca7dd
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 12:32:06 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/CustomerBOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/CustomerBOImpl.java
@@ -1,12 +1,17 @@
 package lk.ijse.javaeethogakade.bo.custom.impl;
 
 import lk.ijse.javaeethogakade.bo.custom.CustomerBO;
+import lk.ijse.javaeethogakade.dao.custom.CustomerDAO;
+import lk.ijse.javaeethogakade.dao.custom.impl.CustomerDAOImpl;
 import lk.ijse.javaeethogakade.dto.CustomerDto;
+import lk.ijse.javaeethogakade.entity.Customer;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CustomerBOImpl implements CustomerBO {
+
+    CustomerDAO customerDAO = new CustomerDAOImpl();
     @Override
     public ArrayList<CustomerDto> getAllCustomers() throws SQLException, ClassNotFoundException {
         return null;
@@ -14,7 +19,7 @@ public class CustomerBOImpl implements CustomerBO {
 
     @Override
     public boolean addCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return customerDAO.add(new Customer(dto.getId(), dto.getName(), dto.getAddress(), dto.getSalary()));
     }
 
     @Override
