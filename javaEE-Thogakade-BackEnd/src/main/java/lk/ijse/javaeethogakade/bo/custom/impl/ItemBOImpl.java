fd91c444c6e4cfb7ea28d218fed48fc94a3885b0
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 12:46:09 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/ItemBOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/bo/custom/impl/ItemBOImpl.java
@@ -1,12 +1,17 @@
 package lk.ijse.javaeethogakade.bo.custom.impl;
 
 import lk.ijse.javaeethogakade.bo.custom.ItemBO;
+import lk.ijse.javaeethogakade.dao.custom.ItemDAO;
+import lk.ijse.javaeethogakade.dao.custom.impl.ItemDAOImpl;
 import lk.ijse.javaeethogakade.dto.ItemDTO;
+import lk.ijse.javaeethogakade.entity.Items;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemBOImpl implements ItemBO {
+
+    ItemDAO itemDAO = new ItemDAOImpl();
     @Override
     public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
         return null;
@@ -14,12 +19,12 @@ public class ItemBOImpl implements ItemBO {
 
     @Override
     public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
-        return false;
+        return itemDAO.delete(code);
     }
 
     @Override
     public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return itemDAO.add(new Items(dto.getCode(), dto.getDescription(), dto.getUnitPrice(), dto.getQtyOnHand()));
     }
 
     @Override
