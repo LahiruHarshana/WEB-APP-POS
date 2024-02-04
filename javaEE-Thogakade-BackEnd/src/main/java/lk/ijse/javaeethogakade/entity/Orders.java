5470bc07fe09bc751b0652c30ca8f62e5c04516a
LahiruHarshana <lharshana2002@gmail.com>
2024-02-04 11:31:45 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/Orders.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/entity/Orders.java
@@ -1,5 +1,10 @@
 package lk.ijse.javaeethogakade.entity;
+import lombok.*;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Orders {
     private String orderID;
     private String orderDate;
