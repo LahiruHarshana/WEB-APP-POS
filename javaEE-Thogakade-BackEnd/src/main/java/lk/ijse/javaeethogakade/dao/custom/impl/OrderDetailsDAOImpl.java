6345c66fc15471d9bf70f2d77d94d3352baa7ebd
LahiruHarshana <lharshana2002@gmail.com>
2024-02-06 12:19:34 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/OrderDetailsDAOImpl.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/custom/impl/OrderDetailsDAOImpl.java
@@ -22,7 +22,7 @@ public class OrderDetailsDAOImpl implements OrderDetailsDAO {
             PreparedStatement pstm = connection.prepareStatement(sql);
             pstm.setString(1,entity.getOrderID());
             pstm.setString(2,entity.getItemCode());
-            pstm.setObject(3,entity.getQty());
+            pstm.setInt(3,entity.getQty());
             pstm.setObject(4,entity.getUnitPrice());
             return pstm.executeUpdate() > 0;
         }
