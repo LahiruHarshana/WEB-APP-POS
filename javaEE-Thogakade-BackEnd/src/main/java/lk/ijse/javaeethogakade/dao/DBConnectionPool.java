4d5878890fbb482def07bec24c94a67d8ff1805f
LahiruHarshana <lharshana2002@gmail.com>
2024-02-06 21:11:46 +0530

    updated

--- javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/DBConnectionPool.java
+++ javaEE-Thogakade-BackEnd/src/main/java/lk/ijse/javaeethogakade/dao/DBConnectionPool.java
@@ -1,25 +1,31 @@
 package lk.ijse.javaeethogakade.dao;
-import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+
 public class DBConnectionPool {
     private static DataSource dataSource;
 
     static {
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/TestThogaKade");
-        basicDataSource.setUsername("root");
-        basicDataSource.setPassword("12345678");
-        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        basicDataSource.setMaxTotal(5);
-        basicDataSource.setMaxIdle(2);
-        basicDataSource.setMaxWaitMillis(-1);
-        dataSource = basicDataSource;
-    }
+        try {
+            // Initialize JNDI context
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
 
+            // Look up the data source by JNDI name
+            dataSource = (DataSource) envContext.lookup("jdbc/TestThogaKade");
+        } catch (NamingException e) {
+            e.printStackTrace(); // Handle exception appropriately
+        }
+    }
     public static Connection getConnection() throws SQLException {
+        if (dataSource == null) {
+            throw new SQLException("DataSource is not initialized.");
+        }
         return dataSource.getConnection();
     }
 }
